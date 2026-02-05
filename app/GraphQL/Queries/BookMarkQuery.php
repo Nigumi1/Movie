c0e07542ee02ca7dcbca7de28a86f4eadbc739e0
JC <johncarlo.catindig006@gmail.com>
2026-02-05 20:05:02 +0800

    added add bookmark

--- app/GraphQL/Queries/BookMarkQuery.php
+++ app/GraphQL/Queries/BookMarkQuery.php
@@ -9,6 +9,8 @@ use GraphQL\Type\Definition\ResolveInfo;
 use GraphQL\Type\Definition\Type;
 use Rebing\GraphQL\Support\Query;
 use Rebing\GraphQL\Support\SelectFields;
+use App\Models\BookMark;
+use Rebing\GraphQL\Support\Facades\GraphQL;
 
 class BookMarkQuery extends Query
 {
@@ -19,18 +21,30 @@ class BookMarkQuery extends Query
 
     public function type(): Type
     {
-        return Type::listOf(Type::string());
+        return Type::listOf(GraphQL::type('bookmark_type'));
     }
 
     public function args(): array
     {
         return [
-
+            'page' => [
+                'type' => Type::int(),
+            ],
+            'perPage' => [
+                'type' => Type::int(),
+            ],
+            'action_type' => [
+                'type' => Type::string(),
+            ]
         ];
     }
 
     public function resolve($root, array $args, $context, ResolveInfo $resolveInfo, Closure $getSelectFields)
     {
-        
+        $bookmarkModel = new BookMark();
+
+        if ($args['action_type'] == 'display_bookmark') {
+            return $bookmarkModel->displayBookMarks($args['page'], $args['perPage']);
+        }
     }
 }
